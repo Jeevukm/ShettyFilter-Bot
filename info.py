5e488a47ff7f2698d6e1c1a9bb551ea28a0ee375
Jeevukm <161112690+Jeevukm@users.noreply.github.com>
2024-03-20 14:06:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -81,12 +81,12 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Files')
 
 # Links
-SUPPORT_LINK = environ.get('SUPPORT_LINK', 'https://t.me/HA_Bots_Support')
-OWNER_USERNAME = environ.get("OWNER_USERNAME", "https://t.me/Hansaka_Anuhas")
-UPDATES_LINK = environ.get('UPDATES_LINK', 'https://t.me/HA_Bots')
-FILMS_LINK = environ.get('FILMS_LINK', 'https://t.me/HA_Films_World')
-TUTORIAL = environ.get("TUTORIAL", "https://t.me/HA_Bots")
-VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/HA_Bots")
+SUPPORT_LINK = environ.get('SUPPORT_LINK', 'https://t.me/shettyDiscus_Group')
+OWNER_USERNAME = environ.get("OWNER_USERNAME", "https://t.me/jeevan_shetty")
+UPDATES_LINK = environ.get('UPDATES_LINK', 'https://t.me/shettymoviess')
+FILMS_LINK = environ.get('FILMS_LINK', 'https://t.me/shetty_moviess')
+TUTORIAL = environ.get("TUTORIAL", "https://t.me/shettymoviess")
+VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/shettymoviess")
 
 # Bot settings
 DELETE_TIME = int(environ.get('DELETE_TIME', 3600)) # Add time in seconds
